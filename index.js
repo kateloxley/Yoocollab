bdfa997ffe7b2a8c47ca525a561108f7d53506d7
Katya <ksloxley@gmail.com>
2022-09-10 12:03:56 +0300

    feat: create header menu

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import './assets/scss/main.scss';
 import './assets/images/logo.svg';
+
+const headerBurger = document.querySelector('.burger');
+const headerMenu = document.querySelector('.menu');
+const headerLinks = document.querySelectorAll('.header__link');
+const headerClose = document.querySelector('.header__close');
+const page = document.querySelector('.page');
+
+const closeMenu = (event) => {
+  if (
+    event.target.classList.contains('header__link') ||
+    event.target.classList.contains('active') ||
+    event.target.classList.contains('header__close')
+  ) {
+    headerMenu.classList.remove('active');
+    headerClose.classList.remove('active');
+    page.classList.remove('noscroll');
+  }
+};
+
+headerBurger.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  headerClose.classList.toggle('active');
+  page.classList.toggle('noscroll');
+});
+
+headerLinks.forEach((el) => el.addEventListener('click', closeMenu));
+
+headerClose.addEventListener('click', closeMenu);
