ed596bafa1b724d86ba474fd21a4599f547f4b7d
Katya <ksloxley@gmail.com>
2022-09-11 20:59:59 +0300

    feat: add about page

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import './assets/images/logo.svg';
 const headerBurger = document.querySelector('.burger');
 const headerMenu = document.querySelector('.menu');
 const headerLinks = document.querySelectorAll('.header__link');
+const headerWhiteLinks = document.querySelectorAll('.menu__link_white');
 const headerClose = document.querySelector('.header__close');
 const page = document.querySelector('.page');
 
 const closeMenu = (event) => {
   if (
     event.target.classList.contains('header__link') ||
+    event.target.classList.contains('header__link_white') ||
     event.target.classList.contains('active') ||
     event.target.classList.contains('header__close')
   ) {
@@ -25,16 +27,50 @@ headerBurger.addEventListener('click', () => {
   page.classList.toggle('noscroll');
 });
 
-headerLinks.forEach((el) => el.addEventListener('click', closeMenu));
+if (headerLinks) {
+  headerLinks.forEach((el) => el.addEventListener('click', closeMenu));
+}
 
 headerClose.addEventListener('click', closeMenu);
 
 const seeMore = document.querySelector('.provided-services__more');
-const openMore = (event) => {
-  document
-    .querySelectorAll('.provided-services__item')
-    .forEach((el) => el.classList.remove('hide'));
-  seeMore.style.display = 'none';
+const openMore = () => {
+  if (seeMore) {
+    document
+      .querySelectorAll('.provided-services__item')
+      .forEach((el) => el.classList.remove('hide'));
+    seeMore.style.display = 'none';
+  }
 };
 
-seeMore.addEventListener('click', openMore);
+if (seeMore) {
+  seeMore.addEventListener('click', openMore);
+}
+
+const more = document.querySelectorAll('.readmore');
+const moreText = document.querySelectorAll('.more');
+
+const readMore = (event) => {
+  if (
+    event.target.classList.contains('readmore') &&
+    event.target.classList.contains('top-banner__more')
+  ) {
+    document.querySelector('.top-banner__more').style.display = 'none';
+    document.querySelector('.dots').style.display = 'none';
+    document
+      .querySelectorAll('.top-banner__text')
+      .forEach((el) => el.classList.remove('more'));
+  }
+  if (
+    event.target.classList.contains('readmore') &&
+    event.target.classList.contains('benefits__more')
+  ) {
+    document.querySelector('.benefits__more').style.display = 'none';
+    document.querySelector('.benefits__dots').style.display = 'none';
+    document
+      .querySelectorAll('.benefits__item-text')
+      .forEach((el) => el.classList.remove('more'));
+  }
+};
+
+more.forEach((el) => el.addEventListener('click', readMore));
