437f6a3b48bb622d678f5eb979d685c095a784ae
Katya <ksloxley@gmail.com>
2022-09-13 20:08:30 +0300

    feat: add team page mobile

--- index.js
+++ index.js
@@ -55,41 +55,86 @@ const readMore = (event) => {
     event.target.classList.contains('readmore') &&
     event.target.classList.contains('top-banner__more')
   ) {
-    document.querySelector('.top-banner__more').style.display = 'none';
-    document.querySelector('.dots').style.display = 'none';
-    document
-      .querySelectorAll('.top-banner__text')
-      .forEach((el) => el.classList.remove('more'));
+    if (
+      document.querySelector('.top-banner__more') &&
+      document.querySelector('.dots') &&
+      document.querySelectorAll('.top-banner__text')
+    ) {
+      document.querySelector('.top-banner__more').style.display = 'none';
+      document.querySelector('.dots').style.display = 'none';
+      document
+        .querySelectorAll('.top-banner__text')
+        .forEach((el) => el.classList.remove('more'));
+    }
   }
   if (
     event.target.classList.contains('readmore') &&
     event.target.classList.contains('benefits__more')
   ) {
-    document.querySelector('.benefits__more').style.display = 'none';
-    document.querySelector('.benefits__dots').style.display = 'none';
-    document
-      .querySelectorAll('.benefits__item-text')
-      .forEach((el) => el.classList.remove('more'));
+    if (
+      document.querySelector('.benefits__more') &&
+      document.querySelector('.benefits__dots') &&
+      document.querySelectorAll('.benefits__item-text')
+    ) {
+      document.querySelector('.benefits__more').style.display = 'none';
+      document.querySelector('.benefits__dots').style.display = 'none';
+      document
+        .querySelectorAll('.benefits__item-text')
+        .forEach((el) => el.classList.remove('more'));
+    }
   }
   if (
     event.target.classList.contains('readmore') &&
     event.target.classList.contains('ideas-banner__more')
   ) {
-    document.querySelector('.ideas-banner__more').style.display = 'none';
-    document.querySelector('.ideas-banner__dots').style.display = 'none';
-    document
-      .querySelectorAll('.ideas-banner__text_mobile')
-      .forEach((el) => el.classList.remove('more'));
+    if (
+      document.querySelector('.ideas-banner__more') &&
+      document.querySelector('.ideas-banner__dots') &&
+      document.querySelectorAll('.ideas-banner__text_mobile')
+    ) {
+      document.querySelector('.ideas-banner__more').style.display = 'none';
+      document.querySelector('.ideas-banner__dots').style.display = 'none';
+      document
+        .querySelectorAll('.ideas-banner__text_mobile')
+        .forEach((el) => el.classList.remove('more'));
+    }
   }
 };
 
-more.forEach((el) => el.addEventListener('click', readMore));
+if (more) {
+  more.forEach((el) => el.addEventListener('click', readMore));
+}
 
-document.querySelector('.contacts__title').addEventListener('click', () => {
-  document
-    .querySelectorAll('.form__error')
-    .forEach((el) => el.classList.add('error'));
-  document
-    .querySelectorAll('.form__input')
-    .forEach((el) => el.classList.add('error'));
-});
+// document.querySelector('.contacts__title').addEventListener('click', () => {
+//   document
+//     .querySelectorAll('.form__error')
+//     .forEach((el) => el.classList.add('error'));
+//   document
+//     .querySelectorAll('.form__input')
+//     .forEach((el) => el.classList.add('error'));
+// });
+
+document.querySelectorAll('.team__col').forEach((el) =>
+  el.addEventListener('click', (event) => {
+    if (event.target.id == 'frontend') {
+      document.querySelector('.frontend').classList.toggle('active');
+      document
+        .querySelector('.team__arrow_frontend')
+        .classList.toggle('active');
+    }
+    if (event.target.id == 'backend') {
+      document.querySelector('.backend').classList.toggle('active');
+      document.querySelector('.team__arrow_backend').classList.toggle('active');
+    }
+    if (event.target.id == 'mobile') {
+      document.querySelector('.mobile').classList.toggle('active');
+      document.querySelector('.team__arrow_mobile').classList.toggle('active');
+    }
+    if (event.target.id == 'database') {
+      document.querySelector('.database').classList.toggle('active');
+      document
+        .querySelector('.team__arrow_database')
+        .classList.toggle('active');
+    }
+  })
+);
