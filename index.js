b1eb4c5ae03a040866a9d40f29bf2a3fe5fa7f87
Katya <ksloxley@gmail.com>
2022-09-13 23:08:38 +0300

    feat: add team page mob

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const headerLinks = document.querySelectorAll('.header__link');
 const headerWhiteLinks = document.querySelectorAll('.menu__link_white');
 const headerClose = document.querySelector('.header__close');
 const page = document.querySelector('.page');
+import onsubmit from './assets/images/onsubmit.png';
 
 const closeMenu = (event) => {
   if (
@@ -138,3 +139,35 @@ document.querySelectorAll('.team__col').forEach((el) =>
     }
   })
 );
+
+document.querySelector('.team__btn').addEventListener('click', (event) => {
+  event.preventDefault();
+  document.querySelector('.contact-details').classList.add('active');
+  page.classList.add('noscroll');
+});
+
+document
+  .querySelector('.contact-details__btn')
+  .addEventListener('click', (event) => {
+    event.preventDefault();
+    document.querySelector(
+      '.contact-details__content'
+    ).innerHTML = `<div class="contact-details__onsubmit-container"> 
+    <span class="contact-details__close"></span>
+    <img src=${onsubmit} alt="" />
+    <p class="contact-details__onsubmit-message">Thank you for choosing us as your trusted source!</p>
+    </div>`;
+  });
+
+const contactDetails = document.querySelector('.contact-details');
+
+contactDetails.addEventListener('click', (event) => {
+  console.log(event.target);
+  if (
+    event.target.classList.contains('active') ||
+    event.target.classList.contains('contact-details__close')
+  ) {
+    contactDetails.classList.remove('active');
+    page.classList.remove('noscroll');
+  }
+});
